05d0ad7815067667e6708ff6f74c719d9f959fe0
yinstardev <prashant.patil@thoughtspot.com>
2025-04-23 18:53:14 +0530

    ut-test

--- Package.swift
+++ Package.swift
@@ -4,7 +4,8 @@ import PackageDescription
 let package = Package(
     name: "iOS-native-embed-sdk",
     platforms: [
-        .iOS(.v14)
+        .iOS(.v14),
+        .macOS(.v11)
     ],
     products: [
         .library(
